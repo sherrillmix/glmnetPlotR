390104596e5b34ebb250f65206530dc17260a61c
sherrillmix <sherrillmix@users.noreply.github.com>
2018-09-17 12:48:34 -0400

    Add xlim and ylim option for plotBetas. Add package::s

--- R/plotGlmnet.R
+++ R/plotGlmnet.R
@@ -25,44 +25,44 @@
 #' plotGlmnet(cvob1)
 plotGlmnet<-function(fit,markBest1SE=FALSE,...){
   extraCex<-.85
-  par(mar=c(4.1,4.8,.5,.5))
+  graphics::par(mar=c(4.1,4.8,.5,.5))
   yRange<-range(c(fit$cvup,fit$cvlo))
   devName<-tolower(fit$name)
   substring(devName,1,1)<-toupper(substring(devName,1,1))
   xRange<-rev(range(log10(fit$lambda)))
-  plot(log10(fit$lambda),fit$cvm,type='n',xlim=xRange,ylim=yRange,ylab=sprintf('%s',devName),pch=21,bg='red',col=NA,las=1,xlab='',yaxt='n',xaxt='n',mgp=c(3.9,1,0),...,cex.lab=1.2)
-  title(xlab=expression(paste('Model complexity (',lambda,')')),mgp=c(3.2,1,0),cex.lab=1.2)
+  graphics::plot(log10(fit$lambda),fit$cvm,type='n',xlim=xRange,ylim=yRange,ylab=sprintf('%s',devName),pch=21,bg='red',col=NA,las=1,xlab='',yaxt='n',xaxt='n',mgp=c(3.9,1,0),...,cex.lab=1.2)
+  graphics::title(xlab=expression(paste('Model complexity (',lambda,')')),mgp=c(3.2,1,0),cex.lab=1.2)
   #text(par('usr')[1]+diff(par('usr')[1:2])*.01,fit$cvm[1],'Random',adj=c(0,.5),col='white')
-  text(par('usr')[1]-diff(par('usr')[1:2])*.011,fit$cvm[1],'Random',adj=c(1,.5),xpd=NA)
+  graphics::text(graphics::par('usr')[1]-diff(graphics::par('usr')[1:2])*.011,fit$cvm[1],'Random',adj=c(1,.5),xpd=NA)
   prettyY<-pretty(yRange,high.u.bias=5)
-  axis(2,prettyY,rep('',length(prettyY)))
+  graphics::axis(2,prettyY,rep('',length(prettyY)))
   prettyY<-prettyY[abs(prettyY-fit$cvm[1])>.003] #no overlap with random
   #axis(2,pretty(yRange),rep('',length(pretty(yRange))),las=1)
-  axis(2,prettyY,las=1)
+  graphics::axis(2,prettyY,las=1)
   prettyX<-pretty(log10(fit$lambda),high.u.bias=90)
-  axis(1,prettyX,sapply(prettyX,function(x)as.expression(bquote(10^.(x)))),las=1)
-  if(markBest1SE)abline(v=log10(c(fit$lambda.1se)),lty=2)#fit$lambda.min,
-  segments(log10(fit$lambda),fit$cvup,log10(fit$lambda),fit$cvlo,col='grey')
+  graphics::axis(1,prettyX,sapply(prettyX,function(x)as.expression(bquote(10^.(x)))),las=1)
+  if(markBest1SE)graphics::abline(v=log10(c(fit$lambda.1se)),lty=2)#fit$lambda.min,
+  graphics::segments(log10(fit$lambda),fit$cvup,log10(fit$lambda),fit$cvlo,col='grey')
   stepsize<-diff(log10(fit$lambda))[1]
-  segments(log10(fit$lambda)-stepsize/2,fit$cvup,log10(fit$lambda)+stepsize/2,fit$cvup,col='grey')
-  segments(log10(fit$lambda)-stepsize/2,fit$cvlo,log10(fit$lambda)+stepsize/2,fit$cvlo,col='grey')
-  points(log10(fit$lambda),fit$cvm,pch=21,bg='red',col='darkred',cex=.6)
-  abline(h=fit$cvm[1],lty=3)
+  graphics::segments(log10(fit$lambda)-stepsize/2,fit$cvup,log10(fit$lambda)+stepsize/2,fit$cvup,col='grey')
+  graphics::segments(log10(fit$lambda)-stepsize/2,fit$cvlo,log10(fit$lambda)+stepsize/2,fit$cvlo,col='grey')
+  graphics::points(log10(fit$lambda),fit$cvm,pch=21,bg='red',col='darkred',cex=.6)
+  graphics::abline(h=fit$cvm[1],lty=3)
   centerPoints<-fit$cvm[1]+diff(yRange)*.04*c(-1,1)
-  vertStrWidth<-strwidth('Better',cex=par('cex.main'))/diff(par('usr')[1:2])*diff(par('usr')[3:4])
+  vertStrWidth<-graphics::strwidth('Better',cex=graphics::par('cex.main'))/diff(graphics::par('usr')[1:2])*diff(graphics::par('usr')[3:4])
   outPoints<-centerPoints+vertStrWidth*c(-1.15,1.15)#diff(yRange)*.30*c(-1,1)
-  mtext('Better',2,line=2.9,at=centerPoints[1],adj=1,cex=extraCex)
-  mtext('Worse',2,line=2.9,at=centerPoints[2],adj=0,cex=extraCex)
+  graphics::mtext('Better',2,line=2.9,at=centerPoints[1],adj=1,cex=extraCex)
+  graphics::mtext('Worse',2,line=2.9,at=centerPoints[2],adj=0,cex=extraCex)
   #xCoord<-par('usr')[1]-diff(par('usr')[1:2])*.1925
   xCoord<-convertLineToUser(2.9,2)
-  arrows(xCoord,centerPoints,xCoord,outPoints,xpd=NA,length=.1)
-  centerPoints<-mean(par('usr')[1:2])+diff(par('usr')[1:2])*.03*c(-1,1)
-  outPoints<-centerPoints+strwidth('Less complex',cex=par('cex.main'))*c(-1.15,1.1) #diff(par('usr')[1:2])*.32*c(-1,1)
-  mtext('Less complex',1,line=1.95,at=centerPoints[1],adj=1,cex=extraCex)
-  mtext('More complex',1,line=1.95,at=centerPoints[2],adj=0,cex=extraCex)
+  graphics::arrows(xCoord,centerPoints,xCoord,outPoints,xpd=NA,length=.1)
+  centerPoints<-mean(graphics::par('usr')[1:2])+diff(graphics::par('usr')[1:2])*.03*c(-1,1)
+  outPoints<-centerPoints+graphics::strwidth('Less complex',cex=graphics::par('cex.main'))*c(-1.15,1.1) #diff(par('usr')[1:2])*.32*c(-1,1)
+  graphics::mtext('Less complex',1,line=1.95,at=centerPoints[1],adj=1,cex=extraCex)
+  graphics::mtext('More complex',1,line=1.95,at=centerPoints[2],adj=0,cex=extraCex)
   #yCoord<-par('usr')[3]-diff(par('usr')[3:4])*.175
   yCoord<-convertLineToUser(2.9,1)
-  arrows(centerPoints,yCoord,outPoints,yCoord,xpd=NA,length=.1)
+  graphics::arrows(centerPoints,yCoord,outPoints,yCoord,xpd=NA,length=.1)
   return(invisible(NULL))
 }
 
@@ -76,6 +76,8 @@ plotGlmnet<-function(fit,markBest1SE=FALSE,...){
 #' @param ylab label for y axis
 #' @param transformFunc a functions to adjust y-axis labels (e.g. \code{exp} to show the axis as e^beta or \code{function(x)x^2} for 2^beta)
 #' @param minBeta the minimum absolute necessary to count beta as different from zero
+#' @param xlim minimum and maximum for limits of the x axis. Leave empty to set automatically
+#' @param ylim minimum and maximum for limits of the y axis. Leave empty to set automatically
 #' @param ... additional arguments for \code{\link{plot}}
 #' @return NULL 
 #' @export
@@ -95,49 +97,49 @@ plotGlmnet<-function(fit,markBest1SE=FALSE,...){
 #' set.seed(1011)
 #' cvob1=glmnet::cv.glmnet(x,y)
 #' plotBetas(cvob1$glmnet.fit,cvob1$lambda.1se)
-plotBetas<-function(glmnet,labelLambda=0,ylab='Coefficient',transformFunc=function(x)x,minBeta=0,...){
-  par(mar=c(4,3.5,.5,.5))
+plotBetas<-function(glmnet,labelLambda=0,ylab='Coefficient',transformFunc=function(x)x,minBeta=0,xlim=rev(range(log10(glmnet$lambda)))+c(0,-.2),ylim=range(betas),...){
+  graphics::par(mar=c(4,3.5,.5,.5))
   nonZeros<-apply(glmnet$beta,1,function(x)any(abs(x)>minBeta))
   betas<-as.matrix(glmnet$beta[nonZeros,,drop=FALSE])
-  cols<-rainbow(nrow(betas),s=.7,alpha=.8)
-  plot(1,1,xlim=rev(range(log10(glmnet$lambda)))+c(0,-.2),ylim=range(betas),xaxt='n',xlab='',las=1,ylab=ylab,...,mgp=c(2.5,1,0),yaxt='n')
-  sapply(1:nrow(betas),function(x)lines(log10(glmnet$lambda),betas[x,],col=cols[x],lwd=2))
+  cols<-grDevices::rainbow(nrow(betas),s=.7,alpha=.8)
+  graphics::plot(1,1,xlim=xlim,ylim=ylim,xaxt='n',xlab='',las=1,ylab=ylab,...,mgp=c(2.5,1,0),yaxt='n')
+  sapply(1:nrow(betas),function(x)graphics::lines(log10(glmnet$lambda),betas[x,],col=cols[x],lwd=2))
   prettyX<-pretty(log10(glmnet$lambda),high.u.bias=90)
-  axis(1,prettyX,sapply(prettyX,function(x)as.expression(bquote(10^.(x)))),las=1)
+  graphics::axis(1,prettyX,sapply(prettyX,function(x)as.expression(bquote(10^.(x)))),las=1)
   prettyY<-pretty(betas,high.u.bias=90)
-  axis(2,prettyY,sub('[.0]+$','',format(transformFunc(prettyY))),las=1)
-  title(xlab=expression(paste('Model complexity (',lambda,')')),mgp=c(3.2,1,0),cex.lab=1.2)
+  graphics::axis(2,prettyY,sub('[.0]+$','',format(transformFunc(prettyY))),las=1)
+  graphics::title(xlab=expression(paste('Model complexity (',lambda,')')),mgp=c(3.2,1,0),cex.lab=1.2)
   if(labelLambda>0){
     selectVars<-which(abs(betas[,max(which(glmnet$lambda>=labelLambda))])>minBeta)
     if(length(selectVars)>0){
       selectVars<-selectVars[order(betas[selectVars,ncol(betas)])]
       varNames<-rownames(betas)[selectVars]
-      yPos<-betas[selectVars,ncol(betas)]+strheight('M')*.2
+      yPos<-betas[selectVars,ncol(betas)]+graphics::strheight('M')*.2
       #yPos<-transformFunc(0)+diff(par('usr')[3:4])*.0075*c(-1,1)[(betas[selectVars,ncol(betas)]<0)+1]
       #xPos<-log10(glmnet$lambda[apply(betas[selectVars,],1,function(x)max(which(x==0)))])
       offsetY<-yPos
       if(length(selectVars)>1){
         for(ii in 2:length(selectVars)){
-          if(offsetY[ii]-offsetY[ii-1]<strheight(varNames[ii])){
-            offsetY[ii]<-min(par('usr')[4]-strheight(varNames[ii]),offsetY[ii-1]+strheight(varNames[ii]))
+          if(offsetY[ii]-offsetY[ii-1]<graphics::strheight(varNames[ii])){
+            offsetY[ii]<-min(graphics::par('usr')[4]-graphics::strheight(varNames[ii]),offsetY[ii-1]+graphics::strheight(varNames[ii]))
           }
-          offsetY[ii]<-min(par('usr')[4]-strheight(varNames[ii]),offsetY[ii]) #keep in plot
+          offsetY[ii]<-min(graphics::par('usr')[4]-graphics::strheight(varNames[ii]),offsetY[ii]) #keep in plot
         }
       }
-      xPos<-par('usr')[2]-diff(par('usr')[1:2])*.005-strwidth(varNames)*.5
+      xPos<-graphics::par('usr')[2]-diff(graphics::par('usr')[1:2])*.005-graphics::strwidth(varNames)*.5
       #xPos<-xPos-diff(par('usr')[1:2])*.02*rep(c(0,1),length.out=length(selectVars))[order(yPos)]
-      text(xPos,offsetY,varNames,adj=c(.5,0),col=cols[selectVars])
-      abline(v=log10(labelLambda),lty=2)
-      segments(xPos,offsetY,log10(glmnet$lambda[ncol(betas)]),betas[selectVars,ncol(betas)],lty=2,col=cols[selectVars])
+      graphics::text(xPos,offsetY,varNames,adj=c(.5,0),col=cols[selectVars])
+      graphics::abline(v=log10(labelLambda),lty=2)
+      graphics::segments(xPos,offsetY,log10(glmnet$lambda[ncol(betas)]),betas[selectVars,ncol(betas)],lty=2,col=cols[selectVars])
     }
   }
-  centerPoints<-mean(par('usr')[1:2])+diff(par('usr')[1:2])*.03*c(-1,1)
-  outPoints<-centerPoints+strwidth('Less complex',cex=par('cex.main'))*c(-1.15,1.1) #diff(par('usr')[1:2])*.32*c(-1,1)
-  mtext('Less complex',1,line=1.95,at=centerPoints[1],adj=1,cex=.85)
-  mtext('More complex',1,line=1.95,at=centerPoints[2],adj=0,cex=.85)
+  centerPoints<-mean(graphics::par('usr')[1:2])+diff(graphics::par('usr')[1:2])*.03*c(-1,1)
+  outPoints<-centerPoints+graphics::strwidth('Less complex',cex=graphics::par('cex.main'))*c(-1.15,1.1) #diff(par('usr')[1:2])*.32*c(-1,1)
+  graphics::mtext('Less complex',1,line=1.95,at=centerPoints[1],adj=1,cex=.85)
+  graphics::mtext('More complex',1,line=1.95,at=centerPoints[2],adj=0,cex=.85)
   #yCoord<-par('usr')[3]-diff(par('usr')[3:4])*.175
   yCoord<-convertLineToUser(2.9,1)
-  arrows(centerPoints,yCoord,outPoints,yCoord,xpd=NA,length=.1)
+  graphics::arrows(centerPoints,yCoord,outPoints,yCoord,xpd=NA,length=.1)
   return(invisible(NULL))
 }
 
@@ -148,12 +150,12 @@ convertLineToUser<-function(line,axis=1){
   axisPair<-sort((c(axis-1,axis+1)%%4)+1)
   isHeight<-(axis%%2)==1
   isSecond<-axis>2
-  thisMar<-par('mar')[axis]
-  marWidth<-thisMar/sum(par('mar')[axisPair])*(par('fin')-par('pin'))[isHeight+1]
+  thisMar<-graphics::par('mar')[axis]
+  marWidth<-thisMar/sum(graphics::par('mar')[axisPair])*(graphics::par('fin')-graphics::par('pin'))[isHeight+1]
   widthPerLine<-marWidth/thisMar
   #find base line + add in if plot doesn't cover whole device e.g. par(mfrow=c(2,1))
-  base<-ifelse(isSecond,par('fin')[isHeight+1]-widthPerLine*thisMar,widthPerLine*thisMar) + par('fig')[1+isHeight*2]*par('din')[isHeight+1]
-  func<-if(isHeight)grconvertY else grconvertX
+  base<-ifelse(isSecond,graphics::par('fin')[isHeight+1]-widthPerLine*thisMar,widthPerLine*thisMar) + graphics::par('fig')[1+isHeight*2]*graphics::par('din')[isHeight+1]
+  func<-if(isHeight)graphics::grconvertY else graphics::grconvertX
   out<-func(base+line*widthPerLine*ifelse(isSecond,1,-1),'inches','user')
   return(out)
 }
